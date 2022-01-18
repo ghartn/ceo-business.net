9edbff0a40fe471686e7658e4fbecf6babf64e90
Garrett <garrett.hartnell@gmail.com>
2022-01-18 09:14:34 -0800

    suhwee

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -36,7 +36,7 @@ class HomePage extends Component {
 	}
 
 	_clickButton = () => {
-		window.open("https://get.ceoatbusiness.net/iuo2", "_blank");
+		window.open("https://get.ceoatbusiness.net/suhwee", "_blank");
 		this.setState({
 			buttonClicked: !this.state.buttonClicked,
 			buttonPosition: {
@@ -67,12 +67,12 @@ class HomePage extends Component {
 				</h1>
 				<div className="pt-16">
 					<BouncingText className="text-xl sm:text-3xl md:text-4xl text-red-pure text-center text-glow">
-						CEO@BUSINESS.NET - INCENTIVIZE UNPAID OVERTIME 2
+						CEO@BUSINESS.NET - SUHWEE
 					</BouncingText>
 					<div className="flex justify-center">
 						<iframe
 							className="mt-6"
-							title="ceo@business.net - take a trip"
+							title="ceo@business.net - suhwee"
 							width="560"
 							height="315"
 							src="https://www.youtube.com/embed/MWXdpxgbndw"
@@ -82,7 +82,7 @@ class HomePage extends Component {
 						></iframe>
 					</div>
 					<BouncingText className="mt-6 text-xl sm:text-3xl md:text-4xl text-blue-pure text-center text-glow">
-						NEW ALBUM OUT NOW
+						NEW SONG OUT NOW
 					</BouncingText>
 				</div>
 				<div className="pt-12 flex items-center">
